5dc2eac13d9ac524ae50a3b3f9a0ff1b0eaa625b
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-03-05 23:30:43 +0530

    Added teacher card table

--- ashaAPI/models.py
+++ ashaAPI/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 # Create your models here.
@@ -11,3 +12,25 @@ class CarouselImage(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+
+class TeacherCard(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100,blank=False,default='')
+    description = models.TextField(blank=False)
+    qualifications =  models.TextField(blank=False)
+
+    def __str__(self):
+        return self.name
+    
+
+class TeacherImage(models.Model):
+    sno = models.AutoField(primary_key=True)
+    teacher = models.OneToOneField(TeacherCard,on_delete=models.CASCADE)
+    image = models.ImageField(blank=False,upload_to='TeacherImages/')
+
+
+    def __str__(self):
+        return self.teacher.name
