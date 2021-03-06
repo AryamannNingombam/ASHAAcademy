26d2fde4619542636d50c82624c7b14157daa140
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-03-06 23:43:52 +0530

    Added Tiny-MCE

--- ashaAPI/admin.py
+++ ashaAPI/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import CarouselImage
+from .models import CarouselImage,TeacherCard,TeacherImage
 # Register your models here.
-admin.site.register(CarouselImage)
+admin.site.register((CarouselImage,TeacherImage))
+
+
+
+
+@admin.register(TeacherCard)
+class DescriptionAdminMCE(admin.ModelAdmin):
+    class Media:
+        js = ('/static/scripts/TinyMCE.js',)
+
+        
+    
