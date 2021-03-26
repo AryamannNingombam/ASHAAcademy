e55fd36924b898f0e6a5b615adf07cd8910c0ba0
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-03-26 10:57:59 +0530

    Added student Model

--- student_portal/models.py
+++ student_portal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class StudentData(models.Model):
+    sno = models.AutoField(primary_key=True)
+    studentMainData = models.OneToOneField(User, on_delete=models.CASCADE)
+    classStudyingIn = models.IntegerField(null=False, blank=False, default=8)
+    studentID = models.CharField(max_length=100, blank=False, default='',unique=True)
+    parentName = models.CharField(max_length=100, blank=False, default='')
+    studentImage = models.ImageField(upload_to='StudentImages/',blank=True,null=True)
+    parentPhoneNumber = models.IntegerField()
