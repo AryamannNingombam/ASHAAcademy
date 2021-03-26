ae3586c7071977cd3328549ecf39f2c8ba0847ad
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-03-26 17:47:05 +0530

    Added auth

--- teacher_portal/models.py
+++ teacher_portal/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from ashaAPI.models import Subject
 from django.contrib.auth.models import User
-
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class TeacherData(models.Model):
@@ -16,6 +19,12 @@ class TeacherData(models.Model):
 
     def __str__(self):
         return self.teacherUserModel.username
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def createAuthToken(sender,instance=None,created=False,**kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 
--- teacher_portal/views.py
+++ teacher_portal/views.py
@@ -2,6 +2,55 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import TeacherData
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from rest_framework.contrib.models import Token
+
+
+
+
+@api_view(['POST'])
+def signInRequest(requests):
+    try:
+        username = requests.POST.get('username')
+        password = requests.POST.get('password')
+        tempCheck = authenticate(username=username,password=password)
+        if (not tempCheck):
+            return JsonResponse({
+                'success' : False,
+                'response' : "Invalid credentials"
+            })
+        else:
+            login(requests, tempCheck)
+            token = Token.objects.get(user=tempCheck).key
+            return JsonResponse({
+                'logged-in': True,
+                'userDetail': {
+                    "username": username,
+                    'password': password,
+                    'token': token,
+                    'teacherImage': tempCheck.teacherImage,
+                    'qualifications': tempCheck.qualifications,
+                    'teacherID': tempCheck.teacherID,
+                    'first_name': tempCheck.teacherUserModel.first_name,
+                    'last_name': tempCheck.teacherUserModel.last_name,
+                    'email': tempCheck.teacherUserModel.email,
+                    'facultySubject': tempCheck.facultysubject.name,
+                    'isInManagement': tempCheck.isInManagement,
+                    'qualifications': tempCheck.qualifications,
+                    'description': tempCheck.description,
+                },
+
+
+            })
+    except Exception as e:
+        return JsonResponse({
+            'logged-in': False,
+            'response': e
+        })
+
+
+
 
 
 @api_view(['GET'])
